fbae33ca7983e68a6db20eed80ff855a0da73c11
annapanjuti <anna.panjuti15@gmail.com>
2023-11-23 14:37:12 +0200

    Update tic-tac-toe.py
    23.11.2023., ceturtdiena, viena stunda

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox #paziņojumi, ieteikumi
 
+
 mansLogs=Tk() #loga objekts
 mansLogs.title("TicTacToe")
 
@@ -9,8 +10,10 @@ mansLogs.title("TicTacToe")
 speletajsX=True
 count=0
 winner=True
+
 #Divi spēlētāji spēlēs vienā datorā
 #Pirmais sāks X, otrais - O
+
 
 #POGU FUNKCIJA
 def btnClick(button): #poda klikšķinās
@@ -30,7 +33,6 @@ def btnClick(button): #poda klikšķinās
     return
 
 
-
 #POGU IZVEIDE, PIEVIENOŠANA (LAUKUMU VEIDO KĀ POGAS)
 btn1=Button(mansLogs,text="",width=6,height=3,font=("Helvica",24),command=lambda:btnClick(btn1)) #pogas izmērs, teksta fonts un strādāšana
 btn2=Button(mansLogs,text="",width=6,height=3,font=("Helvica",24),command=lambda:btnClick(btn2))
@@ -43,6 +45,22 @@ btn8=Button(mansLogs,text="",width=6,height=3,font=("Helvica",24),command=lambda
 btn9=Button(mansLogs,text="",width=6,height=3,font=("Helvica",24),command=lambda:btnClick(btn9))
 
 
+#GALVENĀ IZVĒLNE
+galvenaIzvelne=Menu(mansLogs) #izveido galveno izvēlni
+mansLogs.config(menu=galvenaIzvelne) #pievieno galvenajam logam
+
+
+#MAZĀ IZVĒLNE
+opcijas=Menu(galvenaIzvelne,tearoff=False)#mazā izvelne
+galvenaIzvelne.add_cascade(label="Opcijas",menu=opcijas) #lejupkrītošais saraksts
+
+
+#MAZĀS IZVĒLNES KOMANDAS 
+opcijas.add_command(label="Jauna spēle", command=reset)#jaunas speles sākšanas poga
+opcijas.add_command(label="Iziet",cmmand=mansLogs.quit) #aiztaisa logu, speli
+
+
+
 #POGU PIEVIENOŠANA, NOVIETOJUMS
 btn1.grid(row=0,column=0)
 btn2.grid(row=0,column=1)
@@ -53,6 +71,41 @@ btn6.grid(row=1,column=2)
 btn7.grid(row=2,column=0)
 btn8.grid(row=2,column=1)
 btn9.grid(row=2,column=2)
+
+
+#POGU STĀVOKLIS
+def disableButtons():
+     btn1.config(state=DISABLED)
+     btn2.config(state=DISABLED)
+     btn3.config(state=DISABLED)
+     btn4.config(state=DISABLED)
+     btn5.config(state=DISABLED)
+     btn6.config(state=DISABLED)
+     btn7.config(state=DISABLED)
+     btn8.config(state=DISABLED)
+     btn9.config(state=DISABLED)
+     return 0
+
+def reset():
+     btn1.config(state=NORMAL)
+     btn2.config(state=NORMAL)
+     btn3.config(state=NORMAL)
+     btn4.config(state=NORMAL)
+     btn5.config(state=NORMAL)
+     btn6.config(state=NORMAL)
+     btn7.config(state=NORMAL)
+     btn8.config(state=NORMAL)
+     btn9.config(state=NORMAL)
+
+     btn1["text"]=""
+     btn2["text"]=""
+     btn3["text"]=""
+     btn4["text"]=""
+     btn5["text"]=""
+     btn6["text"]=""
+     btn7["text"]=""
+     btn8["text"]=""
+     btn9["text"]=""
 
 
 #UZVARĒTĀJA FUNKCIJA
@@ -68,6 +121,7 @@ def checkWinner():
         btn2["text"]=="X"and btn5["text"]=="X"and btn8["text"]=="X"or
         btn3["text"]=="X"and btn6["text"]=="X"and btn9["text"]=="X"):
             winner=True
+            disableButtons() #iekavā kaut kas notiek
             messagebox.showinfo("Tic-Tac-Toe","Spēlētājs X ir uzvarētājs!")
     elif (btn1["text"]=="O"and btn2["text"]=="O"and btn3["text"]=="O"or 
         btn4["text"]=="O"and btn5["text"]=="O"and btn6["text"]=="O"or
@@ -78,9 +132,12 @@ def checkWinner():
         btn2["text"]=="O"and btn5["text"]=="O"and btn8["text"]=="O"or
         btn3["text"]=="O"and btn6["text"]=="O"and btn9["text"]=="O"):
             winner=True
+            disableButtons()
             messagebox.showinfo("Tic-Tac-Toe","Spēlētājs O ir uzvarētājs!")
     elif count==9 and winner==False:
+         disableButtons()
          messagebox.showinfo("Tic-Tac-Toe","Spēle beidzās ar neizšķirtu!")
+         return
 
 
 mansLogs.mainloop()
